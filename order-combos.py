80e1dd98c610bb7670c89b6c7ec9ff8b92b87725
BradenMccoy <bmccoy2@uw.edu>
2019-06-27 18:32:54 -0700

    finalized order pair script and fixed orders-raw.txt formatting

--- order-combos.py
+++ order-combos.py
@@ -1,7 +1,15 @@
 file = open("orders-raw.txt", "r")
+output = open("output.txt", "a+")
+
 
 lines = file.readlines()
 for i in lines:
-    print(i)
+    output.write("New Base:\n")
+    for j in lines:
+        output.write(i)
+        
+        output.write(j)
+    output.write("\n")
 
 file.close
+output.close
